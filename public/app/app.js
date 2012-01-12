ad4f9cc9370ba5b66f7aa8c20515da4ae50eca1d
rick.li <racke1983cn@hotmail.com>
2012-01-12 11:28:08 +0800

    replace contents

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var EditVenueView, HomeController, HomeView, Venue, VenueCollection, app,
+  var HomeController, HomeView, Research, ResearchCollection, app,
     __hasProp = Object.prototype.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
@@ -23,103 +23,101 @@
     }
   };
 
-  Venue = (function(_super) {
+  Research = (function(_super) {
 
-    __extends(Venue, _super);
+    __extends(Research, _super);
 
-    function Venue() {
-      Venue.__super__.constructor.apply(this, arguments);
+    function Research() {
+      Research.__super__.constructor.apply(this, arguments);
     }
 
-    Venue.prototype.getName = function() {
-      return this.get('name');
+    Research.prototype.getHeadLine = function() {
+      return this.get('headLine');
     };
 
-    Venue.prototype.getAddress = function() {
-      return [this.get('address'), this.get('city'), this.get('state')].join(", ");
+    Research.prototype.getPubDate = function() {
+      return this.get('pubDate');
     };
 
-    Venue.prototype.getImageUrl = function() {
-      return this.get('photo_url');
+    Research.prototype.getFileLink = function() {
+      return this.get('fileLink');
     };
 
-    Venue.prototype.getLatitude = function() {
-      return this.get('geolat');
+    Research.prototype.getSynopsis = function() {
+      return this.get('synopsis');
     };
 
-    Venue.prototype.getLongtitude = function() {
-      return this.get('geolong');
-    };
-
-    Venue.prototype.getMapUrl = function(width, height) {
-      if (width == null) width = 300;
-      if (height == null) height = 220;
-      return "http://maps.google.com/maps/api/staticmap?center=" + (this.getLatitude()) + "," + (this.getLongitude()) + "&zoom=14&size=" + width + "x" + height + "&maptype=terrain&markers=color:red|" + (this.getLatitude()) + "," + (this.getLongitude()) + "&sensor=false";
-    };
-
-    return Venue;
+    return Research;
 
   })(Backbone.Model);
 
-  VenueCollection = (function(_super) {
+  ResearchCollection = (function(_super) {
 
-    __extends(VenueCollection, _super);
+    __extends(ResearchCollection, _super);
 
-    VenueCollection.prototype.model = Venue;
+    ResearchCollection.prototype.model = Research;
 
-    function VenueCollection() {
-      VenueCollection.__super__.constructor.apply(this, arguments);
-      this.reset($FOURSQUARE_JSON);
+    function ResearchCollection() {
+      ResearchCollection.__super__.constructor.apply(this, arguments);
+      this.reset($RESEARCH_JSON.store.data.list);
     }
 
-    return VenueCollection;
+    return ResearchCollection;
 
   })(Backbone.Collection);
 
-  window.Venues = new VenueCollection;
+  window.Researchs = new ResearchCollection;
 
-  console.log(Venues);
+  console.log(Researchs);
 
-  EditVenueView = (function(_super) {
-
-    __extends(EditVenueView, _super);
-
-    function EditVenueView() {
-      this.render = __bind(this.render, this);      EditVenueView.__super__.constructor.apply(this, arguments);
-      this.el = app.activePage();
-      this.template = _.template('<form action="venue-<%=venue.cid %>-update" method="post">\n  <div data-role="fieldcontain">\n    <label>Name</label>\n    <input type="text" value="<%= venue.getName()%>" name="name" />\n  <div>\n  <div data-role="fieldcontain">\n    <label>Address</label>\n    <input type="text" value="<%= venue.get(\'address\')%>" name="name" />\n  <div>\n  <div data-role="fieldcontain">\n    <label>City</label>\n    <input type="text" value="<%= venue.get(\'city\')%>" name="name" />\n  <div>\n  <div data-role="fieldcontain">\n    <label>State</label>\n    <input type="text" value="<%= venue.get(\'sate\')%>" name="name" />\n  <div>\n  <button type="submit" data-role="button">Save</button>\n</form>');
-      this.model.bind('change', this.render);
-      this.render();
-    }
-
-    EditVenueView.prototype.events = {
-      "submit form": "onSubmit"
-    };
-
-    EditVenueView.prototype.onSubmit = function(e) {
-      this.model.set({
-        name: this.$("input[name='name']").val(),
-        address: this.$("input[name='address']").val(),
-        city: this.$("input[name='city']").val(),
-        state: this.$("input[name='state']").val()
-      });
-      this.model.trigger('change');
-      app.goBack();
-      e.preventDefault;
-      return e.stopPropagation;
-    };
-
-    EditVenueView.prototype.render = function() {
-      this.el.find('h1').text("Editing " + (this.model.getName()));
-      this.el.find('.ui-content').html(this.template({
-        venue: this.model
-      }));
-      return this.delegateEvents;
-    };
-
-    return EditVenueView;
-
-  })(Backbone.View);
+  /*
+  class EditResearchView extends Backbone.View
+    constructor: ->
+      super
+      @el = app.activePage()
+      
+      @template = _.template('''
+      <form action="venue-<%=venue.cid %>-update" method="post">
+        <div data-role="fieldcontain">
+          <label>Name</label>
+          <input type="text" value="<%= venue.getName()%>" name="name" />
+        </div>
+        <div data-role="fieldcontain">
+          <label>State</label>
+          <input type="text" value="<%= venue.get('sate')%>" name="name" />
+        </div>
+        <button type="submit" data-role="button">Save</button>
+      </form>
+      ''')
+      @model.bind 'change', @render
+    
+      @render()
+    events: 
+      "submit form" : "onSubmit"
+  
+  
+    onSubmit: (e) ->
+      @model.set {
+        name : @$("input[name='name']").val(),
+        address : @$("input[name='address']").val(),
+        city : @$("input[name='city']").val(),
+        state : @$("input[name='state']").val()
+      }
+      @model.trigger('change')
+  
+      app.goBack()
+  
+      e.preventDefault
+      e.stopPropagation
+  
+  
+    render: =>
+      @el.find('h1').text("Editing #{@model.getName()}")
+      @el.find('.ui-content').html(@template({venue: @model}))
+      
+      #rebind events
+      @delegateEvents
+  */
 
   HomeView = (function(_super) {
 
@@ -128,15 +126,15 @@
     function HomeView() {
       this.render = __bind(this.render, this);      HomeView.__super__.constructor.call(this);
       this.el = app.activePage();
-      this.template = _.template('<div>\n<ul data-role="listview" data-theme="c" data-filter="true">\n    <% venues.each(function(venue){ %>\n        <li><a href="#venues-<%= venue.cid%>"><%=venue.getName()%></a></li>\n    <%})%>\n</ul>\n</div>');
+      this.template = _.template('<div>\n<ul data-role="listview" data-theme="c" data-filter="true">\n    <% researches.each(function(research){ %>\n        <li><a href="#researches-<%= research.pubId%>"><%=research.getHeadLine()%></a></li>\n    <%})%>\n</ul>\n</div>');
       console.log('home view render');
-      console.log(window.Venues);
+      console.log(window.Researchs);
       this.render();
     }
 
     HomeView.prototype.render = function() {
       this.el.find('.ui-content').html(this.template({
-        venues: window.Venues
+        researches: window.Researchs
       }));
       return app.reapplyStyles(this.el);
     };
@@ -151,7 +149,7 @@
 
     HomeController.prototype.routes = {
       "home": "home",
-      "venues-:cid": "show"
+      "researchs-:cid": "show"
     };
 
     function HomeController() {
