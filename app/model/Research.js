df9b4340beed3409bbe074a09fc7a3d5e87c1fa7
rick.li <racke1983cn@hotmail.com>
2012-02-17 13:53:24 +0800

    add sass style

--- app/model/Research.js
+++ app/model/Research.js
@@ -13,7 +13,7 @@
       {
         pubId: 309040,
         headLine: 'Malaysia Macro Flash: Dec CPI Confirms Disinflationary Trend',
-        synopsis: 'Dec CPI inflation moderated to 3.0%YoY (Nov: 3.3%). This was slightly below consensus and our expectations for 3.1%YoY. MoM inflation remained stable from Nov at 0.1%MoM NSA. This brings inflation for the full year 2011 to 3.2% (2010: 1.7%).',
+        synopsis: 'adfadfadfadfdaf',
         fileLink: 'http://www.education.gov.yk.ca/pdf/pdf-test.pdf'
       }, {
         pubId: 311205,
@@ -23,11 +23,11 @@
       }, {
         pubId: 311255,
         headLine: 'Canada Weekly Notes: Jan 30 - Feb 3',
-        synopsis: 'Canada Weekly Notes: Jan 30 - Feb 3  Industry-Based Output - Activity likely improved modestly in November. Industrial Product Prices - Producer price inflation probably slowed. Raw Materials Prices - Crude price inflation likely continued to decelerate. CFIB Small Business Survey - Capex and pricing plans probably softened. LFS Employment - Job gains likely narrowed keeping UR at recent high.'
+        synopsis: 'dfadfadf'
       }, {
         pubId: 311264,
         headLine: 'Colombia Macro View: What to Expect from Banrep in 2012?',
-        synopsis: "Colombia Minister of Finance Juan Carlos Echeverry announced new FX intervention measures last week after the central bank's monthly meeting. The communique stated the MOF would not bring any more USD from offshore accounts into the country in 2011, and that the MoF would purchase US1.2 bln in the local market to create a fund abroad to be used for future USD-denominated debt obligations. It also stated the MoF would support the CB, if needed, for curbing further COP appreciation.  We looked at possible scenarios for the MoF cash flow through 2011 to assess if the govt has enough resources to undertake the scheme.",
+        synopsis: "adsfdafadfadfadf",
         fileLink: 'http://www.education.gov.yk.ca/pdf/pdf-test.pdf'
       }, {
         pubId: 311249,
