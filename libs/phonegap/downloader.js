045134b7a99f74102e6d16cab2a42274804830ee
ric-g <ric.g21@gmail.com>
2012-02-22 18:58:00 +0800

    revert phonegap

--- libs/phonegap/downloader.js
+++ libs/phonegap/downloader.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 function Downloader() {}
 
 Downloader.prototype.downloadFile = function(fileUrl, params, win, fail) {
@@ -13,33 +11,3 @@ PhoneGap.addConstructor(function() {
 	PhoneGap.addPlugin("downloader", new Downloader());
 	PluginManager.addService("Downloader", "com.phonegap.plugins.downloader.Downloader");
 });
-=======
-function Downloader() {}
-
-Downloader.prototype.downloadFile = function(fileUrl, params, win, fail) {
-	
-	//Make params hash optional.
-	if (!fail) win = params;
-	PhoneGap.exec(win, fail, "Downloader", "downloadFile", [fileUrl, params]);
-};
-
-PhoneGap.addConstructor(function() {
-	PhoneGap.addPlugin("downloader", new Downloader());
-	PluginManager.addService("Downloader", "com.phonegap.plugins.downloader.Downloader");
-});
->>>>>>> b7764ef3faf1b4899c3ec58583d8e0bf86b0b3d8
-=======
-function Downloader() {}
-
-Downloader.prototype.downloadFile = function(fileUrl, params, win, fail) {
-	
-	//Make params hash optional.
-	if (!fail) win = params;
-	PhoneGap.exec(win, fail, "Downloader", "downloadFile", [fileUrl, params]);
-};
-
-PhoneGap.addConstructor(function() {
-	PhoneGap.addPlugin("downloader", new Downloader());
-	PluginManager.addService("Downloader", "com.phonegap.plugins.downloader.Downloader");
-});
->>>>>>> b7764ef3faf1b4899c3ec58583d8e0bf86b0b3d8
